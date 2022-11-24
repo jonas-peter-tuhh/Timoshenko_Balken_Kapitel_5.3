5e36034f82f31dfde313a840f39ffd5b90fdef32
jonas <jonas.peter@tuhh.de>
2022-11-24 12:16:35 +0100

    Mit Normfactor

--- Timoshenko Kragarm.py
+++ Timoshenko Kragarm.py
@@ -18,20 +18,8 @@ from scipy.interpolate import splrep, splev
 import math
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-import scipy.integrate
-import sympy as sy
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-import scipy as sp
-import scipy.integrate as integrate
-from scipy.integrate import quad
-import scipy.special as special
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.interpolate import splrep, splev
-import math
 
+train = True
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -55,35 +43,53 @@ class Net(nn.Module):
         layer7_out = torch.tanh(self.hidden_layer7(layer6_out))
         output = self.output_layer(layer7_out)
         return output
-
+##
+choice_load = input("Möchtest du ein State_Dict laden? y/n")
+if choice_load == 'y':
+    train=False
+    filename = input("Welches State_Dict möchtest du laden?")
+    net = Net()
+    net = net.to(device)
+    net.load_state_dict(torch.load('C:\\Users\\Administrator\\Desktop\\Uni\\Master\\Masterarbeit\\Timoshenko NN Kragarm 5.3\\saved_data\\'+filename))
+    net.eval()
+##
 # Hyperparameter
 learning_rate = 0.0075
-
+if train:
+    net = Net()
+    net = net.to(device)
 net_B = Net()
 net_S = Net()
 net_B = net_B.to(device)
 net_S = net_S.to(device)
 mse_cost_function = torch.nn.MSELoss()  # Mean squared error
 optimizer = torch.optim.Adam([{'params': net_B.parameters()}, {'params': net_S.parameters()}], lr=learning_rate)
-#Der Scheduler sorgt dafür, dass die Learning Rate auf einem Plateau mit dem factor multipliziert wird
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=200, verbose=True, factor= 0.8)
 
 # Definition der Parameter des statischen Ersatzsystems
+
 Lb = float(input('Länge des Kragarms [m]: '))
-EI = float(input('EI des Balkens [10^6 kNcm²]: '))
-A = float(input('Querschnittsfläche des Balkens [cm²]: '))
-G = float(input('Schubmodul des Balkens [GPa]: '))
-LFS = int(input('Anzahl Streckenlasten: '))
+E = 21#float(input('E-Modul des Balkens [10^6 kNcm²]: '))
+h = 10#float(input('Querschnittshöhe des Balkens [cm]: '))
+b = 10#float(input('Querschnittsbreite des Balkens [cm]: '))
+A = h*b
+I = (b*h**3)/12
+EI = E*I*10**-3
+G = 80#float(input('Schubmodul des Balkens [GPa]: '))
+LFS = 1#int(input('Anzahl Streckenlasten: '))
 K = 5 / 6  # float(input(' Schubkoeffizient '))
 Ln = np.zeros(LFS)
 Lq = np.zeros(LFS)
 s = [None] * LFS
+normfactor = 10/(Lb**3/(K*A*G)+(11*Lb**5)/(120*EI))
+
+#Der Scheduler sorgt dafür, dass die Learning Rate auf einem Plateau mit dem factor multipliziert wird
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=200, verbose=True, factor=0.75)
 
 for i in range(LFS):
     # ODE als Loss-Funktion, Streckenlast
-    Ln[i] = float(input('Länge Einspannung bis Anfang der ' + str(i + 1) + '. Streckenlast [m]: '))
-    Lq[i] = float(input('Länge der ' + str(i + 1) + '. Streckenlast [m]: '))
-    s[i] = input(str(i + 1) + '. Streckenlast eingeben: ')
+    Ln[i] = 0#float(input('Länge Einspannung bis Anfang der ' + str(i + 1) + '. Streckenlast [m]: '))
+    Lq[i] = Lb#float(input('Länge der ' + str(i + 1) + '. Streckenlast [m]: '))
+    s[i] = str(normfactor)+"*x"#input(str(i + 1) + '. Streckenlast eingeben: ')
 
 def h(x, j):
     return eval(s[j])
@@ -97,8 +103,8 @@ def f(x, net_B):
     u_xxxx = torch.autograd.grad(u_xxx, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(u))[0]
     ode = 0
     for i in range(LFS):
+        #0 = vb'''' + q(x)/EI
         ode += u_xxxx + h(x - Ln[i], i) / EI  * (x <= (Ln[i] + Lq[i])) * (x >= Ln[i])
-        # 0 = w''''(x) - q(x)/EI + q''(x)/KAG
     return ode
 
 #Netzwerk für Schub
@@ -106,11 +112,13 @@ def g(x, net_S):
     u = net_S(x)
     u_x = torch.autograd.grad(u, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(u))[0]
     u_xx = torch.autograd.grad(u_x, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(u))[0]
-    ode = u_xx - h(x - Ln[i], i) / (K * A * G *10**-1) * (x <= (Ln[i] + Lq[i])) * (x >= Ln[i])
+    #0 = vs'' - q(x)/KAG
+    ode = u_xx - h(x - Ln[i], i) / (K * A * G ) * (x <= (Ln[i] + Lq[i])) * (x >= Ln[i])
     return ode
 
 
 x = np.linspace(0, Lb, 1000)
+pt_x = torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=True).to(device), 1)
 qx = np.zeros(1000)
 for i in range(LFS):
     qx = qx + (h(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1) - Ln[i], i).cpu().detach().numpy()).squeeze() * (x <= (Ln[i] + Lq[i])) * (x >= Ln[i])
@@ -121,16 +129,19 @@ qxx = qx * x
 #M0 = M(0) = int(q(x)*x), über den ganzen Balken
 M0 = integrate.cumtrapz(qxx, x, initial=0)
 #Die nächsten Zeilen bis Iterationen geben nur die Biegelinie aus welche alle 10 Iterationen refreshed wird während des Lernens, man kann also den Lernprozess beobachten
-y1 = net_B(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1)) #+ net_S(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1))
-fig = plt.figure()
-plt.grid()
-ax = fig.add_subplot()
-ax.set_xlim([0, Lb])
-ax.set_ylim([-30, 0])
-line1, = ax.plot(x, y1.cpu().detach().numpy())
-
-iterations = 15000
+if train:
+    y1 = net_B(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1)) + net_S(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1))
+    fig = plt.figure()
+    plt.grid()
+    ax = fig.add_subplot()
+    ax.set_xlim([0, Lb])
+    ax.set_ylim([-11, 0])
+    line1, = ax.plot(x, y1.cpu().detach().numpy())
+    f_anal=(-1/120 * normfactor * pt_x**5 + 1/6 * Q0[-1] * pt_x**3 - M0[-1]/2 *pt_x**2)/EI + (1/6 * normfactor * (pt_x)**3 - Q0[-1]*pt_x)/(K*A*G)
+##
+iterations = 1000000
 for epoch in range(iterations):
+    if not train: break
     optimizer.zero_grad()  # to make the gradients zero
     x_bc = np.linspace(0, Lb, 500)
     # linspace x Vektor zwischen 0 und 1, 500 Einträge gleichmäßiger Abstand
@@ -173,17 +184,18 @@ for epoch in range(iterations):
 
     #RB für Schub
     e1_S = net_bc_out_S[0]
-    e2_S = u_x_S[0] + Q0[-1]/(K*A*G*10**-1)
+    e2_S = u_x_S[0] + Q0[-1]/(K*A*G)
     e3_S = u_x_S[-1]
 
     #Alle e's werden gegen 0-Vektor (pt_zero) optimiert.
 
-    mse_bc_B = mse_cost_function(e1_B, pt_zero) + mse_cost_function(e2_B, pt_zero) + mse_cost_function(e3_B, pt_zero) + mse_cost_function(e4_B, pt_zero) + mse_cost_function(e5_B, pt_zero) + mse_cost_function(e6_B, pt_zero)
-    mse_ode_B = mse_cost_function(ode_B, pt_all_zeros)
-    mse_bc_S = mse_cost_function(e1_S, pt_zero) + mse_cost_function(e2_S, pt_zero) + mse_cost_function(e3_S, pt_zero)
-    mse_ode_S = mse_cost_function(ode_S, pt_all_zeros)
+    mse_bc_B = mse_cost_function(e1_B, pt_zero) + mse_cost_function(e2_B, pt_zero) + 1/normfactor * mse_cost_function(e3_B, pt_zero) + 1/normfactor * mse_cost_function(e4_B, pt_zero) + mse_cost_function(e5_B, pt_zero) + mse_cost_function(e6_B, pt_zero)
+    mse_ode_B = 1/normfactor * mse_cost_function(ode_B, pt_all_zeros)
+    mse_bc_S = mse_cost_function(e1_S, pt_zero) + 1/normfactor * mse_cost_function(e2_S, pt_zero) + mse_cost_function(e3_S, pt_zero)
+    mse_ode_S = 1/normfactor * mse_cost_function(ode_S, pt_all_zeros)
 
-    loss = 3*mse_ode_S + 3*mse_ode_B + mse_bc_S + mse_bc_B
+    loss = mse_ode_S + mse_ode_B + mse_bc_S + mse_bc_B
+    loss = 1 / normfactor * loss
 
     loss.backward()
     optimizer.step()
@@ -192,9 +204,21 @@ for epoch in range(iterations):
         if epoch % 10 == 9:
             print(epoch, "Traning Loss:", loss.data)
             plt.grid()
+            net_out = net_B(pt_x) + net_S(pt_x)
+            err = torch.norm(net_out - f_anal, 2)
+            print(f'Error = {err}')
+            if err < 0.1*Lb:
+                print(f"Die L^2 Norm des Fehlers ist {err}.\nStoppe Lernprozess")
+                break
             line1.set_ydata(net_B(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device),1)).cpu().detach().numpy() + net_S(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device),1)).cpu().detach().numpy())
             fig.canvas.draw()
             fig.canvas.flush_events()
+##
+if choice_load == 'n':
+    choice_save = input("Möchtest du die Netzwerkparameter abspeichern? y/n")
+    if choice_save == 'y':
+        filename = input("Wie soll das State_Dict heißen?")
+        torch.save(net.state_dict(),'C:\\Users\\Administrator\\Desktop\\Uni\\Master\\Masterarbeit\\Timoshenko NN Kragarm 5.3\\saved_data\\'+filename)
 ##
 pt_x = torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=True).to(device), 1)
 
@@ -217,9 +241,9 @@ ws_x = ws_x.cpu().detach().numpy()
 ws_xx = ws_xx.cpu().detach().numpy()
 
 #Biegung Kompatibilität Numpy Array
-u_out_cpu = pt_u_out.cpu()
-u_out = u_out_cpu.detach()
-u_out = u_out.numpy()
+v_out_cpu = pt_u_out.cpu()
+v_out = v_out_cpu.detach()
+v_out = v_out.numpy()
 
 #Schub Kompatibilität Numpy Array
 s_out_cpu = pt_u_out_s.cpu()
@@ -234,36 +258,24 @@ plt.subplot(3, 2, 1)
 plt.title('$v_{b}$ Auslenkung (aus Biegung)')
 plt.xlabel('')
 plt.ylabel('[cm]')
-plt.plot(x, u_out)
-plt.plot(x, (-1/1800 * x**6 + 36/15 * x**3 - 162/5 *x**2)/EI)
-#plt.plot(x, (-7/1200 * x**5 + 343/120 * x**3 - 2401/60 *x**2)/EI)
-#plt.plot(x, (-np.sin(x) - 1/8 * x**4 + (Q0[-1]-1)/6 * x**3 - M0[-1]/2 * x**2 + x)/EI)
-#plt.plot(x, (-1/120 * x**5 + 12.5/6 * x**3 - 41.67/2 * x**2)/EI)
-# plt.plot(x, (-1/120 * x**5 + 1/12 * x**3 -1/6 * x**2)/EI - x**3 * 1/6 * 1/(K*A*G))
-# plt.plot(x, ((-0.2/360 * x**6 + 8.333/6 * x**3 - 31.25/2 * x**2))/EI)#0.2*x**2
-# plt.plot(x, (np.cos(x)- 1/8 * x**4 + 23.99/6 * x**3 - 71.95/2 * x**2)/EI)#3+cos(x)
+plt.plot(x, v_out)
+plt.plot(x, (-1/120 * normfactor * x**5 + Q0[-1]/6 * x**3 - M0[-1]/2 * x**2)/EI)
 plt.grid()
 
 plt.subplot(3, 2, 3)
-plt.title('$\phi$ Neigung (aus Biegung)')
+plt.title('$\phi$ Neigung')
 plt.xlabel('')
 plt.ylabel('$10^{-2}$')
 plt.plot(x, w_x)
-plt.plot(x, (-1/300 * x**5 +36/5 *x**2 - 64.8 * x)/EI)
-#plt.plot(x, (-1/2 * x**3 - np.cos(x) + (Q0[-1]-1)/2 *x**2 - M0[-1] * x + 1)/EI)
-#plt.plot(x, (-1/80 * x**4 + 147/40 * x**2 - 34.3 * x)/EI)# 0.7*x, 7m
-# plt.plot(x, (-3*x + np.cos(x) + 18.31)/EI)#3+sin(x)
-# plt.plot(x, (np.sin(x) - 3*x + 23.99)/EI)#3+cos(x)
+plt.plot(x, (-1/24 * normfactor * x**4 + 0.5 * Q0[-1] * x**2 - M0[-1] * x)/EI)
 plt.grid()
 
 plt.subplot(3, 2, 5)
-plt.title('$\kappa$ Krümmung (aus Biegung)')
+plt.title('$\kappa$ Krümmung')
 plt.xlabel('Meter')
 plt.ylabel('$(10^{-4})$[1/cm]')
 plt.plot(x, w_xx)
-plt.plot(x, (-1/60 * x**4 + 14.4 * x - 64.8)/EI)
-#plt.plot(x, (-3/2 * x**2 + np.sin(x) + (Q0[-1]-1) * x - M0[-1] )/EI)
-#plt.plot(x,  -h(x - Ln[i], i) / EI )
+plt.plot(x, ( - 1/6 * normfactor * x**3 + Q0[-1]*x - M0[-1])/EI)
 plt.grid()
 
 #Timoshenko Plots
@@ -271,10 +283,9 @@ plt.grid()
 plt.subplot(3, 2, 2)
 plt.title('$v_{s}$ Auslenkung (aus Schub)')
 plt.xlabel('')
-plt.ylabel('$mm$')
+plt.ylabel('$cm$')
 plt.plot(x, s_out)
-#plt.plot(x, (1/20 * x**3 - 7.35 * x)/(K*A*G*10**-1))
-plt.plot(x, (1/60 * x**4 - 14.4 * x)/(K*A*G*10**-1))
+plt.plot(x, (1/6 * normfactor* x**3 - Q0[-1] * x)/(K*A*G))
 plt.grid()
 
 plt.subplot(3, 2, 4)
@@ -282,24 +293,13 @@ plt.title('Schubwinkel $\gamma$')
 plt.xlabel('')
 plt.ylabel('$(10^{-2})$')
 plt.plot(x, ws_x)
-#plt.plot(x, (3/20 * x**2 - 7.35)/(K*A*G*10**-1))
-#plt.plot(x, (0.5*x**2 - 12.5)/(K*A*G*10**-1))
-plt.plot(x, (1/15 * x**3 - 14.4)/(K*A*G*10**-1))
-# plt.plot(x, (-0.2/60 * x**5 + 8.333/2 * x**2 - 31.25 * x)/EI) #0.2*x**2#
-# plt.plot(x, (-np.sin(x) - 0.5*x**3 + 23.99/2 * x**2 - 71.95/2 * x)/EI)#cos(x)+3
+plt.plot(x, (normfactor * 0.5 * x**2 - Q0[-1])/(K*A*G))
 plt.grid()
 
-plt.subplot(3, 2, 6)
-plt.title('Krümmung aus Schub $\gamma´$')
-plt.xlabel('Meter')
-plt.ylabel('$(10^{-4})$[1/mm]')
-plt.plot(x, ws_xx)
-plt.plot(x, (0.2*x**2)/(K*A*G*10**-1))
-#plt.plot(x, (3+np.sin(x))/(K*A*G*10**-1))
-# plt.plot(x, (-3/2 * x**2 + np.sin(x) + 18.31 * x - 55.26)/EI)#sin(x)+3
-# plt.plot(x, ((-0.2/12 * x**4 + 8.333 * x - 31.25 ))/EI)#0.2*x**2
-# plt.plot(x, (-np.cos(x) - 3/2 * x**2 + 23.99*x - 71.95)/EI)
-plt.grid()
+gamma_anal = ((normfactor * 0.5 * x**2 - Q0[-1])/(K*A*G))
+gamma_net = ws_x
+gamma_err = np.linalg.norm((gamma_net-gamma_anal), 2)/np.linalg.norm(gamma_anal, 2)
+print('\u03B3 5.1 =',gamma_err)
 
 plt.show()
 ##
